c4ba797cdbbac7601635aa6259053736bbe6f06f
nehir-mentes <nehirmen.98@gmail.com>
2025-04-24 19:09:15 +0000

    draft

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,7 +1,53 @@
 class ZebraController < ApplicationController
   
   def homepage
-    render({:template => "zebra/homepage"})
+    render({:template => "zebra/new_square"})
   end
 
+  def square
+    @the_num = params.fetch("number").to_f
+    @the_square = @the_num ** 2
+    render({:template => "zebra/square_results"})
+  end  
+
+  def new_square_root
+    render({:template => "zebra/new_square_root"})
+  end 
+
+  def square_root
+    @the_num = params.fetch("number").to_f
+    @the_square = @the_num ** 0.5
+    render({:template => "zebra/square_root_results"})
+  end
+
+  def new_payment
+    render({:template => "zebra/new_payment"})
+  end 
+
+  def payment
+    @user_apr = params.fetch("user_apr").to_f.to_fs(:percentage, { :precision => 4 } )
+    @user_years = params.fetch("user_years").to_f
+    @user_pv = params.fetch("user_pv").to_f.to_fs(:currency)
+    
+    r_term = (params.fetch("user_apr").to_f / 100 / 12)
+    nominator = r_term * params.fetch("user_pv").to_f
+    loan_term = params.fetch("user_years").to_f * 12
+    denominator = 1-((1+r_term) ** (-1*loan_term)) 
+  
+    @the_payment = (nominator / denominator).to_fs(:currency)
+
+    render({:template => "zebra/payment_results"})
+  end
+
+  def new_random
+    render({:template => "zebra/new_random"})
+  end
+
+  def random
+    @user_min = params.fetch("user_min").to_f
+    @user_max = params.fetch("user_max").to_f
+    @random_number = rand(@user_min..@user_max)
+
+    render({:template => "zebra/random_results"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,19 @@ Rails.application.routes.draw do
   get("/", { :controller => "zebra", :action => "homepage"})
 
   get("/square/new", { :controller => "zebra", :action => "homepage"})
+
+  get("/square/results", { :controller => "zebra", :action => "square"})
+
+  get("/square_root/new", { :controller => "zebra", :action => "new_square_root"})
+
+  get("/square_root/results", { :controller => "zebra", :action => "square_root"})
+
+  get("/payment/new", { :controller => "zebra", :action => "new_payment"})
+
+  get("/payment/results", { :controller => "zebra", :action => "payment"})
+
+  get("/random/new", { :controller => "zebra", :action => "new_random"})
+
+  get("/random/results", { :controller => "zebra", :action => "random"})
+
 end
